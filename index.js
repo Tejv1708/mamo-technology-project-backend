714f5af8b27dd9752b6ad479f3fcbe6dc4694eb8
Tejv1708 <narukatejveer@gmail.com>
2025-01-13 10:51:23 +0530

    second commit

--- index.js
+++ index.js
@@ -9,21 +9,8 @@ dotenv.config("./env");
 
 const app = express();
 app.use(express.json());
-const corsOptions = {
-  origin: "http://localhost:5174",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
 
-app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5174");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(cors());
 
 app.use("/api/user", userRouter);
 app.use("/api/time", timeRoutes);
